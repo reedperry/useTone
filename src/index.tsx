8fda8d76e4602bd97fcd60798c9ccfc25f7ecbc8
Reed Perry <reedperry@users.noreply.github.com>
2018-11-21 13:43:26 -0700

    Trying more flexible API for play/stop status of useTone

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-function useTone(type = 'square', frequency = 440, gain = 10) {
-  const [playing, setPlaying] = React.useState(false);
+function useTone(playing = false, type = 'square', frequency = 440, gain = 10) {
   const gainNode = React.useRef(null);
   const osc = React.useRef(null);
   const audioContext = React.useRef(null);
@@ -20,7 +19,7 @@ function useTone(type = 'square', frequency = 440, gain = 10) {
     osc.current = oscillator;
 
     return () => {
-      osc.current.stop();
+      osc.current.stop(0);
       osc.current.disconnect();
     };
   }, []);
@@ -44,46 +43,44 @@ function useTone(type = 'square', frequency = 440, gain = 10) {
     },
     [playing]
   );
-
-  return [playing, setPlaying];
 }
 
 const App = () => {
   const [pitch, setPitch] = React.useState(440);
-  const [gain, setGain] = React.useState(10);
-  const [sinePlaying, sineSetPlaying] = useTone('sine', pitch, gain);
-  const [squarePlaying, squareSetPlaying] = useTone(
-    'square',
-    pitch * 1.5,
-    gain
-  );
-  const [square2Playing, square2SetPlaying] = useTone(
-    'square',
-    pitch * 0.75,
-    gain
-  );
+  const [gain, setGain] = React.useState(0.5);
+  const [allPlaying, setAllPlaying] = React.useState(false);
+  const [voice1Playing, setVoice1Playing] = React.useState(false);
+  const [voice2Playing, setVoice2Playing] = React.useState(false);
+  const [voice3Playing, setVoice3Playing] = React.useState(false);
+  useTone(allPlaying || voice1Playing, 'sine', pitch, gain);
+  useTone(allPlaying || voice2Playing, 'square', pitch * 1.5, gain);
+  useTone(allPlaying || voice3Playing, 'sawtooth', pitch * 0.75, gain);
   return (
     <div>
       <input
         type="range"
-        min="60"
-        max="3000"
+        min="30"
+        max="1500"
         value={pitch}
         onChange={e => setPitch(e.target.value)}
       />
-      <button onClick={() => sineSetPlaying(!sinePlaying)}>Play/Pause</button>
-      <button onClick={() => squareSetPlaying(!squarePlaying)}>
+      <button onClick={() => setVoice1Playing(!voice1Playing)}>
         Play/Pause
       </button>
-      <button onClick={() => square2SetPlaying(!square2Playing)}>
+      <button onClick={() => setVoice2Playing(!voice2Playing)}>
         Play/Pause
       </button>
+      <button onClick={() => setVoice3Playing(!voice3Playing)}>
+        Play/Pause
+      </button>
+      <button onClick={() => setAllPlaying(!allPlaying)}>Play/Pause All</button>
       <div style={{ marginTop: 80 }}>
         <input
           type="range"
           style={{ transform: 'rotateZ(-90deg)' }}
           min="0"
-          max="100"
+          max="1.0"
+          step="0.01"
           value={gain}
           onChange={e => setGain(e.target.value)}
         />
